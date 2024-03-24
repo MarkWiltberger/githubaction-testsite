81dc93b5527f048ab3f4950619c4dde108fa102e
Mark Wiltberger <mark.wiltberger@gmail.com>
2024-03-24 18:47:17 -0400

    try don't destructure context

--- github-actions/move-closed-issues/sort-closed-issues-test.js
+++ github-actions/move-closed-issues/sort-closed-issues-test.js
@@ -2,7 +2,7 @@ const obtainLabels = require("../utils/obtain-labels");
 
 //   const labelsObject = context.payload.issue.labels;
 
-function main({ context }) {
+function main(context) {
   console.log("run obtainLabels");
   //  console.log(context.payload.issue.labels);
   const issueLabels = obtainLabels(context);
