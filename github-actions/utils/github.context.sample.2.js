08e0a53d68fb5125f9b441b726f4e51e72a4bdcb
Mark Wiltberger <mark.wiltberger@gmail.com>
2024-03-24 19:05:37 -0400

    try console logging context

---
+++ github-actions/utils/github.context.sample.2.js
@@ -0,0 +1,163 @@
+// one example of github context object
+Context = {
+  payload: {
+    inputs: null,
+    ref: "refs/heads/main",
+    repository: {
+      allow_forking: true,
+      archive_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/{archive_format}{/ref}",
+      archived: false,
+      assignees_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/assignees{/user}",
+      blobs_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/git/blobs{/sha}",
+      branches_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/branches{/branch}",
+      clone_url: "https://github.com/MarkWiltberger/githubaction-testsite.git",
+      collaborators_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/collaborators{/collaborator}",
+      comments_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/comments{/number}",
+      commits_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/commits{/sha}",
+      compare_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/compare/{base}...{head}",
+      contents_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/contents/{+path}",
+      contributors_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/contributors",
+      created_at: "2023-02-19T19:53:20Z",
+      default_branch: "main",
+      deployments_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/deployments",
+      description:
+        "private repository to test githubactions and learn how they work",
+      disabled: false,
+      downloads_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/downloads",
+      events_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/events",
+      fork: false,
+      forks: 0,
+      forks_count: 0,
+      forks_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/forks",
+      full_name: "MarkWiltberger/githubaction-testsite",
+      git_commits_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/git/commits{/sha}",
+      git_refs_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/git/refs{/sha}",
+      git_tags_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/git/tags{/sha}",
+      git_url: "git://github.com/MarkWiltberger/githubaction-testsite.git",
+      has_discussions: false,
+      has_downloads: true,
+      has_issues: true,
+      has_pages: false,
+      has_projects: true,
+      has_wiki: false,
+      homepage: null,
+      hooks_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/hooks",
+      html_url: "https://github.com/MarkWiltberger/githubaction-testsite",
+      id: 603868859,
+      is_template: false,
+      issue_comment_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/issues/comments{/number}",
+      issue_events_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/issues/events{/number}",
+      issues_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/issues{/number}",
+      keys_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/keys{/key_id}",
+      labels_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/labels{/name}",
+      language: "JavaScript",
+      languages_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/languages",
+      license: null,
+      merges_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/merges",
+      milestones_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/milestones{/number}",
+      mirror_url: null,
+      name: "githubaction-testsite",
+      node_id: "R_kgDOI_5Ouw",
+      notifications_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/notifications{?since,all,participating}",
+      open_issues: 8,
+      open_issues_count: 8,
+      owner: [Object],
+      private: true,
+      pulls_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/pulls{/number}",
+      pushed_at: "2024-03-24T16:05:42Z",
+      releases_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/releases{/id}",
+      size: 8013,
+      ssh_url: "git@github.com:MarkWiltberger/githubaction-testsite.git",
+      stargazers_count: 0,
+      stargazers_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/stargazers",
+      statuses_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/statuses/{sha}",
+      subscribers_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/subscribers",
+      subscription_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/subscription",
+      svn_url: "https://github.com/MarkWiltberger/githubaction-testsite",
+      tags_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/tags",
+      teams_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/teams",
+      topics: [],
+      trees_url:
+        "https://api.github.com/repos/MarkWiltberger/githubaction-testsite/git/trees{/sha}",
+      updated_at: "2024-03-24T16:05:22Z",
+      url: "https://api.github.com/repos/MarkWiltberger/githubaction-testsite",
+      visibility: "private",
+      watchers: 0,
+      watchers_count: 0,
+      web_commit_signoff_required: false,
+    },
+    sender: {
+      avatar_url: "https://avatars.githubusercontent.com/u/17149108?v=4",
+      events_url:
+        "https://api.github.com/users/MarkWiltberger/events{/privacy}",
+      followers_url: "https://api.github.com/users/MarkWiltberger/followers",
+      following_url:
+        "https://api.github.com/users/MarkWiltberger/following{/other_user}",
+      gists_url: "https://api.github.com/users/MarkWiltberger/gists{/gist_id}",
+      gravatar_id: "",
+      html_url: "https://github.com/MarkWiltberger",
+      id: 17149108,
+      login: "MarkWiltberger",
+      node_id: "MDQ6VXNlcjE3MTQ5MTA4",
+      organizations_url: "https://api.github.com/users/MarkWiltberger/orgs",
+      received_events_url:
+        "https://api.github.com/users/MarkWiltberger/received_events",
+      repos_url: "https://api.github.com/users/MarkWiltberger/repos",
+      site_admin: false,
+      starred_url:
+        "https://api.github.com/users/MarkWiltberger/starred{/owner}{/repo}",
+      subscriptions_url:
+        "https://api.github.com/users/MarkWiltberger/subscriptions",
+      type: "User",
+      url: "https://api.github.com/users/MarkWiltberger",
+    },
+    workflow: ".github/workflows/move-closed-issues-test.yaml",
+  },
+  eventName: "workflow_dispatch",
+  sha: "df5bcff50b3831b42d7a6fb3cd52b316cec1d83d",
+  ref: "refs/heads/main",
+  workflow: "Move Closed Issues Test",
+  action: "sort-closed-issues-test",
+  actor: "MarkWiltberger",
+  job: "move-closed-issue-test",
+  runNumber: 2,
+  runId: 8410551859,
+  apiUrl: "https://api.github.com",
+  serverUrl: "https://github.com",
+  graphqlUrl: "https://api.github.com/graphql",
+};
