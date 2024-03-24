ab61b0e7025f24d1af100fae8241b302da02452e
Mark Wiltberger <mark.wiltberger@gmail.com>
2024-03-24 19:30:21 -0400

    run obtain-labels-inttest

--- github-actions/move-closed-issues/sort-closed-issues-test.js
+++ github-actions/move-closed-issues/sort-closed-issues-test.js
@@ -1,10 +1,10 @@
-// const obtainLabels = require("../utils/obtain-labels-inttest");
+const obtainLabels = require("../utils/obtain-labels-inttest");
 
 //   const labelsObject = context.payload.issue.labels;
 
 function main({ context }) {
   //console.log("console log context");
-  // const issueLabels = obtainLabels(context);
+  const issueLabels = obtainLabels(context);
 }
 
 module.exports = main;
