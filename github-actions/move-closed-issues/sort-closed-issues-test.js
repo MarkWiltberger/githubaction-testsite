81819e9867f96676ae0a89ed33326f0e71707977
Mark Wiltberger <mark.wiltberger@gmail.com>
2024-03-24 18:54:28 -0400

    try console log of context in sort closed issues

--- github-actions/move-closed-issues/sort-closed-issues-test.js
+++ github-actions/move-closed-issues/sort-closed-issues-test.js
@@ -2,7 +2,7 @@ const obtainLabels = require("../utils/obtain-labels");
 
 //   const labelsObject = context.payload.issue.labels;
 
-function main(context) {
+function main({ context }) {
   console.log("console log context");
   console.log(context);
   // const issueLabels = obtainLabels(context);
