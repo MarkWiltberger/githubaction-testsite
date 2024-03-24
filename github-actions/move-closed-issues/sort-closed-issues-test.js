314bae434d5ca73762fffea8783d164a5b64294e
Mark Wiltberger <mark.wiltberger@gmail.com>
2024-03-24 18:45:44 -0400

    try running obtainLabels

--- github-actions/move-closed-issues/sort-closed-issues-test.js
+++ github-actions/move-closed-issues/sort-closed-issues-test.js
@@ -1,12 +1,11 @@
-// const obtainLabels = require('../utils/obtain-labels')
+const obtainLabels = require("../utils/obtain-labels");
 
 //   const labelsObject = context.payload.issue.labels;
 
 function main({ context }) {
-  console.log("create a labels object");
+  console.log("run obtainLabels");
   //  console.log(context.payload.issue.labels);
-
-  const labelsObject = context.payload.issue.labels;
+  const issueLabels = obtainLabels(context);
 }
 
 module.exports = main;
